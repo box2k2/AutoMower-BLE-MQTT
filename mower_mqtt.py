2194542862057db12f5734b7c3c37a987899e061
box2k2 <127965718+box2k2@users.noreply.github.com>
2026-07-08 12:55:54 +0100

    Update mower_mqtt.py
    add logic to escape status loop

--- mower_mqtt.py
+++ mower_mqtt.py
@@ -3,7 +3,7 @@
 
 #  mower_mqtt.py by Andy Brown https://github.com/andyb2000/AutoMower-BLE-MQTT/
 # ------------------------------------------------------------------------------
-VERSION = "0.0.10"
+VERSION = "0.0.11"
 
 import asyncio
 import subprocess
@@ -317,17 +317,20 @@ async def main():
 
                 async def status_loop():
                     while True:
-                        await asyncio.sleep(POLL_INTERVAL)
-                        current_status = await collect_status(mower)
-                        if current_status:
-                            # Access the set via the dictionary key
-                            new_keys = set(current_status.keys()) - context["known_keys"]
-                            if new_keys:
-                                LOG.info("New sensors detected: %s", new_keys)
-                                await ha_discovery(client, current_status)
-                                context["known_keys"].update(new_keys)
-                            
-                            await client.publish(f"{MQTT_BASE_TOPIC}/status", json.dumps(current_status))
+                        try:
+                            await asyncio.sleep(POLL_INTERVAL)
+                            current_status = await collect_status(mower)
+                            if current_status:
+                                new_keys = set(current_status.keys()) - context["known_keys"]
+                                if new_keys:
+                                    LOG.info("New sensors detected: %s", new_keys)
+                                    await ha_discovery(client, current_status)
+                                    context["known_keys"].update(new_keys)
+                                
+                                await client.publish(f"{MQTT_BASE_TOPIC}/status", json.dumps(current_status))
+                        except Exception as e:
+                            # Let the error escape the background loop and bubble out to your global handler!
+                            raise e
 
                 status_task = asyncio.create_task(status_loop())
 
